ce73b5a7d19e222696014aef5c8411afc371d04f
LouBNielsen <louise.b.nielsen@hotmail.com>
2018-04-30 10:15:58 +0200

    Frontend

--- ProjectGutenberg/src/main/java/REST/API.java
+++ ProjectGutenberg/src/main/java/REST/API.java
@@ -9,9 +9,7 @@ import com.google.gson.Gson;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriInfo;
 
 /**
  *
