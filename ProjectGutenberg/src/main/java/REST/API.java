f639eec181ede8ab4f39a5ae264439c44d037d4b
LouBNielsen <louise.b.nielsen@hotmail.com>
2018-04-30 10:49:57 +0200

    Working api

--- ProjectGutenberg/src/main/java/REST/API.java
+++ ProjectGutenberg/src/main/java/REST/API.java
@@ -1,5 +1,23 @@
 package REST;
 
+import com.google.gson.Gson;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("api")
+
 public class API {
-       
+
+    public API() {
+    }
+
+    @GET
+    @Path("test")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String test() {
+        return new Gson().toJson("Hej");
+    }
+
 }
