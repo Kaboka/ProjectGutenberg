17dd210827a988aaeec57609c57e6afe59d04c96
LouBNielsen <louise.b.nielsen@hotmail.com>
2018-05-04 11:37:39 +0200

    working backend with postgre

--- ProjectGutenberg/src/main/java/Connectors/PostgreSQLConnector.java
+++ ProjectGutenberg/src/main/java/Connectors/PostgreSQLConnector.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Connectors;
 
 import java.sql.Connection;
@@ -11,11 +6,50 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-/**
- *
- * @author louis
- */
 public class PostgreSQLConnector {
+
+    public static void main(String[] args) {
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        String query = "SELECT id, author_name\n"
+                + "	FROM \"schemaGutenberg\".author;";
+
+        try {
+
+            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/gutenberg", "postgres", "1234");
+
+            st = con.createStatement();
+            rs = st.executeQuery(query);
+
+            if (rs.next()) {
+                System.out.println(rs.getString(1) + rs.getString(2));
+            }
+            DriverManager driverManager = null;
+            
+            getBook(con, driverManager, st, rs);
+
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+            System.err.println(ex);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (st != null) {
+                    st.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                System.err.println(ex.getMessage());
+                System.err.println(ex);
+            }
+        }
+    }
 
     public static void DBConnector(Connection connection, DriverManager driverManager) {
         try {
@@ -31,10 +65,11 @@ public class PostgreSQLConnector {
         connection.close();
     }
 
-    public void getBook(Connection connection, DriverManager driverManager, Statement statement, ResultSet resultSet) throws SQLException {
+    public static void getBook(Connection connection, DriverManager driverManager, Statement statement, ResultSet resultSet) throws SQLException {
         DBConnector(connection, driverManager);
         statement = connection.createStatement();
-        resultSet = statement.executeQuery("SELECT * FROM book.book");
+        resultSet = statement.executeQuery("SELECT id, author_name\n"
+                + "	FROM \"schemaGutenberg\".author;");
         while (resultSet.next()) {
             System.out.println(resultSet.getString(1) + ", " + resultSet.getString(2));
         }
