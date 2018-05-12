0306332f8d6d7d61892baf04e9573e82e099e43c
LouBNielsen <louise.b.nielsen@hotmail.com>
2018-05-12 13:21:44 +0200

    SQL queries done

--- ProjectGutenberg/src/main/java/Connectors/PostgreSQLConnector.java
+++ ProjectGutenberg/src/main/java/Connectors/PostgreSQLConnector.java
@@ -1,61 +1,18 @@
 package Connectors;
 
 import DataAccess.PostgreSQLDataAccess;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class PostgreSQLConnector {
 
-    public static void main(String[] args) {
-        Connection con = null;
-        Statement st = null;
-        ResultSet rs = null;
-
-        try {
-
-            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/gutenberg", "postgres", "1234");
-
-            st = con.createStatement();
-
-            DriverManager driverManager = null;
-            
-            PostgreSQLDataAccess.getByCityName(con, driverManager, st, rs);
-
-        } catch (SQLException ex) {
-            System.err.println(ex.getMessage());
-            System.err.println(ex);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-                if (st != null) {
-                    st.close();
-                }
-                if (con != null) {
-                    con.close();
-                }
-            } catch (SQLException ex) {
-                System.err.println(ex.getMessage());
-                System.err.println(ex);
-            }
-        }
-    }
-
-    public static void DBConnector(Connection connection, DriverManager driverManager) {
-        try {
-            connection = driverManager.getConnection("jdbc:postgresql://localhost:5432/gutenberg", "postgres", "1234");
-        } catch (SQLException ex) {
-            ex.toString();
-        }
-    }
-
-    public static void DBConnectorClose(Connection connection, Statement statement, ResultSet resultSet) throws SQLException {
-        resultSet.close();
-        statement.close();
-        connection.close();
+    public static void main(String[] args) throws SQLException {
+        System.out.println("Get book title and author name by city name");
+        PostgreSQLDataAccess.getBookAuthorByCity();
+        System.out.println("-----------------------------------");
+        System.out.println("Get city name by book title");
+        PostgreSQLDataAccess.getCityByBook();
+        System.out.println("-----------------------------------");
+        System.out.println("Get book title and author name and city name by author name");
+        PostgreSQLDataAccess.getBookAuthorCityByAuthor();
     }
 }
