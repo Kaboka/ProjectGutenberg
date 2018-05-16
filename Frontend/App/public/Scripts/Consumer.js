4efadef1b0ca69225a089060c8951e859c7cff2d
Emilie <ehn1994@gmail.com>
2018-05-16 12:43:34 +0200

    Updated search from + table

--- Frontend/App/public/Scripts/Consumer.js
+++ Frontend/App/public/Scripts/Consumer.js
@@ -1,11 +1,54 @@
-$(document).ready(function() {
+/*$(document).ready(function () {
     $.ajax({
         url: "http://localhost:8080/ProjectGutenberg/api/api/getBookAuthorByCity"
-    }).then(function(data) {
+    }).then(function (data) {
         $('.dataTable').append(
             $.map(data, function (item, index) {
-                return '<tr><td>' + data[index].author + '</td><td>' + data[index].title + '</td></tr>';
-        }).join());
+                return '<tr><td>' + data[index].author_name + '</td><td>' + data[index].book_title + '</td></tr>';
+            }).join());
     });
-    
+
 });
+$(function () {
+    $("#submit").on("click", function () {
+        var input = $("#cityInput").val();
+        console.log(input);
+        $.ajax({
+            url: "http://localhost:8080/ProjectGutenberg/api/api/getBookAuthorByCity/" + input
+        }).then(function (data) {
+            $('.dataTable').append(
+                $.map(data, function (item, index) {
+                    return '<tr><td>' + data[index].author_name + '</td><td>' + data[index].book_title + '</td></tr>';
+                }).join());
+        });
+    });
+});
+*/
+function load() {
+    var input = $("#cityInput").val();
+    console.log(input);
+    $.ajax({
+        url: "http://localhost:8080/ProjectGutenberg/api/api/getBookAuthorByCity/" + input,
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            $('.dataTable .tbody').empty();
+            $('#error').append("Could not find any results");
+        }
+    }).then(function (data) {
+        console.log(data);
+        if (data.length === 0) {
+            console.log("EMPTY");
+            $('#error').append("Could not find any results");
+            $('.dataTable .tbody').empty();
+        }
+        else {
+            $('.dataTable .tbody').empty().append(
+                $.map(data, function (item, index) {
+                    return '<tr><td>' + data[index].author_name + '</td><td>' + data[index].book_title + '</td></tr>';
+                }).join());
+            $('#error').empty();
+
+        }
+    });
+
+
+}
