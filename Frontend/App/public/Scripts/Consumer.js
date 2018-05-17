c3bf17bcbe5edcb70ab58892c196ed20d82fc0c7
Emilie <ehn1994@gmail.com>
2018-05-17 18:03:38 +0200

    Cucumber test added for user story 1, AC1-4

--- Frontend/App/public/Scripts/Consumer.js
+++ Frontend/App/public/Scripts/Consumer.js
@@ -1,29 +1,3 @@
-/*$(document).ready(function () {
-    $.ajax({
-        url: "http://localhost:8080/ProjectGutenberg/api/api/getBookAuthorByCity"
-    }).then(function (data) {
-        $('.dataTable').append(
-            $.map(data, function (item, index) {
-                return '<tr><td>' + data[index].author_name + '</td><td>' + data[index].book_title + '</td></tr>';
-            }).join());
-    });
-
-});
-$(function () {
-    $("#submit").on("click", function () {
-        var input = $("#cityInput").val();
-        console.log(input);
-        $.ajax({
-            url: "http://localhost:8080/ProjectGutenberg/api/api/getBookAuthorByCity/" + input
-        }).then(function (data) {
-            $('.dataTable').append(
-                $.map(data, function (item, index) {
-                    return '<tr><td>' + data[index].author_name + '</td><td>' + data[index].book_title + '</td></tr>';
-                }).join());
-        });
-    });
-});
-*/
 function load() {
     var input = $("#cityInput").val();
     console.log(input);
@@ -31,14 +5,14 @@ function load() {
         url: "http://localhost:8080/ProjectGutenberg/api/api/getBookAuthorByCity/" + input,
         error: function (XMLHttpRequest, textStatus, errorThrown) {
             $('.dataTable .tbody').empty();
-            $('#errorText').append("Could not find any results");
+            $('#errorText').empty().append("Could not find any results");
             console.log(XMLHttpRequest.error);
         }
     }).then(function (data) {
         console.log(data);
         if (data.length === 0) {
             console.log("EMPTY");
-            $('#errorText').append("Could not find any results");
+            $('#errorText').empty().append("Could not find any results");
             $('.dataTable .tbody').empty();
         }
         else {
