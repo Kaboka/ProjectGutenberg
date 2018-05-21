f2be8469ccee79cb28e26ad58f3f17b9a1a68163
Emilie <ehn1994@gmail.com>
2018-05-21 17:10:04 +0200

    Refactored to use prepared statement in getBookAuthorByCity

--- ProjectGutenberg/src/main/java/DataAccess/PostgreSQLDataAccess.java
+++ ProjectGutenberg/src/main/java/DataAccess/PostgreSQLDataAccess.java
@@ -20,15 +20,9 @@ public class PostgreSQLDataAccess implements DataAccessInterface {
 
     @Override
     public ArrayList<Book> getBookAuthorByCity(String city_name) throws SQLException {
-
-        Statement statement;
         ResultSet resultSet;
-
         ArrayList<Book> books = new ArrayList();
-
-        statement = connection.createStatement();
-        // String city_name = "London";
-        resultSet = statement.executeQuery("SELECT book_title, author_name\n"
+        String query = "SELECT book_title, author_name\n"
                 + "	FROM \"schemaGutenberg\".book AS book \n"
                 + "	INNER JOIN \"schemaGutenberg\".\"book-author\" AS book_author\n"
                 + "	ON (book.id = book_author.book_id)\n"
@@ -38,23 +32,24 @@ public class PostgreSQLDataAccess implements DataAccessInterface {
                 + "	ON (book.id = book_city.book_id)\n"
                 + "	INNER JOIN  \"schemaGutenberg\".city AS city\n"
                 + "	ON (book_city.city_id = city.id)\n"
-                + "	WHERE city.city_name LIKE " + "'" + city_name + "%'");
+                + "	WHERE city.city_name LIKE ?";
 
-        while (resultSet.next()) {
-            books.add(new Book(resultSet.getString(1), resultSet.getString(2)));
+        
+        try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+            pstmt.setString(1, city_name + "%");
+            resultSet = pstmt.executeQuery();
+            
+            while (resultSet.next()) {
+                books.add(new Book(resultSet.getString(1), resultSet.getString(2)));
+            }
+            resultSet.close();
         }
-
-        resultSet.close();
-        statement.close();
         connection.close();
-
         return books;
     }
 
     @Override
     public ArrayList<City> getCitiesByBookTitle(String book_title) throws SQLException {
-        System.out.println("IN DATA ACCESS METHOD");
-        Statement statement;
         ResultSet resultSet;
 
         ArrayList<City> cities = new ArrayList();
@@ -72,8 +67,6 @@ public class PostgreSQLDataAccess implements DataAccessInterface {
             while (resultSet.next()) {
                 cities.add(new City(resultSet.getString(1),resultSet.getFloat(2),resultSet.getFloat(3)));
             }
-            
-            
         }
         
         resultSet.close();
